982070c1382f36ba17d1f1ecada70d8e82cfc4d8
Sho <sho84.kitzxo@gmail.com>
2025-02-02 16:17:45 +0900

    TODOリストを追加できるように

--- js/index.js
+++ js/index.js
@@ -1,3 +1,56 @@
-document.addEventListener("DOMContentLoaded", () => {
+let todoList = [];
 
+// 新たに入力された要素を取得し、todoListに代入する関数
+const registerNewTodo = () => {
+  const todoNameElem = document.getElementById("new-todo-name");
+  const personElem = document.getElementById("new-person");
+  const deadlineElem = document.getElementById("new-deadline");
+  // 新たなTODOリストを追加
+  todoList.push({
+    todoName: todoNameElem.value,
+    person: personElem.value,
+    deadline: deadlineElem.value,
+  });
+};
+
+// todoリスト追加時に、todoの重複を避けるためにtodo一覧を削除する関数
+const removeTodoElem = () => {
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+};
+
+// todoリストの一覧を表示する関数
+const appendTodoListElem = () => {
+  todoList.forEach((todo) => {
+    // todoNameのtdを作成
+    const todoNameTdElem = document.createElement("td");
+    todoNameTdElem.textContent = todo.todoName;
+    // personのtdを作成
+    const personTdElem = document.createElement("td");
+    personTdElem.textContent = todo.person;
+    // deadlineのtdを作成
+    const deadlineTdElem = document.createElement("td");
+    deadlineTdElem.textContent = todo.deadline;
+
+    const trElem = document.createElement("tr");
+    trElem.appendChild(todoNameTdElem);
+    trElem.appendChild(personTdElem);
+    trElem.appendChild(deadlineTdElem);
+
+    const tbodyElem = document.getElementById("tbody");
+    tbodyElem.appendChild(trElem);
+  });
+};
+
+// todoリストの追加するボタンが押されたとき
+document.addEventListener("DOMContentLoaded", () => {
+  const registerButtonElem = document.getElementById("register");
+  registerButtonElem.addEventListener("click", () => {
+    registerNewTodo();
+
+    removeTodoElem();
+
+    appendTodoListElem();
+  });
 });
